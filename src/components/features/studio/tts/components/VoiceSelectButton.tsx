5e1ac21c88dadf5976690684fe27b2b15f45b121
Sai <benshui08@gmail.com>
2025-11-17 23:20:51 +0700

    优化

--- src/components/features/studio/tts/components/VoiceSelectButton.tsx
+++ src/components/features/studio/tts/components/VoiceSelectButton.tsx
@@ -52,12 +52,12 @@ export default function VoiceSelectButton({
       flag: 'w-4 h-4',
     },
     large: {
-      container: 'p-4',
-      avatar: 'w-12 h-12',
-      name: 'text-base',
+      container: 'p-3',
+      avatar: 'w-11 h-11',
+      name: 'text-sm',
       details: 'text-xs',
       icon: 'w-5 h-5',
-      flag: 'w-4 h-4',
+      flag: 'w-3.5 h-3.5',
     },
   };
 

e3c128b50b9e669ad910b2a9d3dfad7329dcd98d
Ben <109528653+benshui08@users.noreply.github.com>
2025-11-20 03:49:15 +0700

    优化

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,7 +3,7 @@
  *
  * 在开发环境中防止热重载创建多个实例
  */
-import { PrismaClient } from '@/generated/prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;

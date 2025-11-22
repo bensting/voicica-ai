adf4866b0107e09286f50f2475d5e4948fcb3506
Sai <benshui08@gmail.com>
2025-11-22 23:38:19 +0700

    优化

--- src/components/layout/Navbar/Logo.tsx
+++ src/components/layout/Navbar/Logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 export default function Logo() {
   return (
-    <Link href="/" className="flex items-center hover:opacity-80 transition-opacity md:-ml-2">
+    <Link href="/" className="flex items-center hover:opacity-80 transition-opacity">
       <Image
         src="/logo/voice-labs-logo-dark.svg"
         alt="AI-Voice-Labs.com"

8805cd0745eb5c0fd65e761288326e6ab441c29d
Sai <benshui08@gmail.com>
2025-11-23 10:25:56 +0700

    优化

--- prisma/migrations/20241123_voice_sample_url_to_json/migration.sql
+++
@@ -1,7 +0,0 @@
--- 将 voice_sample_url 从 text 转换为 json 格式
--- 现有数据转换为 {"default": 原有url} 格式
-
--- 1. 先将现有的 text 列转换为 json，并将数据包装为 {"default": url} 格式
-ALTER TABLE voices
-ALTER COLUMN voice_sample_url TYPE json
-USING jsonb_build_object('default', voice_sample_url)::json;

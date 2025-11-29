74c64c7e5435adbd659ae159b2e14023bae7f4ac
Sai <benshui08@gmail.com>
2025-11-29 15:54:12 +0700

    优化

--- src/config/creditsCost/creditsCost.development.ts
+++ src/config/creditsCost/creditsCost.development.ts
@@ -10,6 +10,6 @@ import type { CreditsCostConfig } from './types';
 export const creditsCostConfig: CreditsCostConfig = {
   [ProductType.TEXT_TO_SPEECH]: 0, // TTS 按字符数计费，由 calculateVoiceCost 计算
   [ProductType.VOICE_CLONING]: 0, // 语音克隆待定
-  [ProductType.YOUTUBE_DOWNLOADER]: 0, // 开发环境不消耗积分
-  [ProductType.TIKTOK_DOWNLOADER]: 0, // 开发环境不消耗积分
+  [ProductType.YOUTUBE_DOWNLOADER]: 1, // 开发环境不消耗积分
+  [ProductType.TIKTOK_DOWNLOADER]: 1, // 开发环境不消耗积分
 };

de99c834a0ed390f4d31eacfc3f600e50149573f
Sai <benshui08@gmail.com>
2025-11-30 14:48:59 +0700

    优化

---
+++ src/components/icons/TTSIcon.tsx
@@ -0,0 +1,19 @@
+/**
+ * Text to Speech Icon Component (Microphone)
+ */
+export default function TTSIcon({ className }: { className?: string }) {
+  return (
+    <svg
+      className={className}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z" />
+    </svg>
+  );
+}

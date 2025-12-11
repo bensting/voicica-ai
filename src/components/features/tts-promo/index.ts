da13e47b46821bbcbf4c336c2e1a627cc6eefa6f
Sai <benshui08@gmail.com>
2025-12-11 21:55:08 +0700

    优化

---
+++ src/components/features/tts-promo/index.ts
@@ -0,0 +1,8 @@
+/**
+ * TTS Promo 落地页公共组件
+ */
+export { default as VoiceSampleGrid } from './VoiceSampleGrid';
+export { default as LanguageDropdown } from './LanguageDropdown';
+export type { LanguageOption } from './LanguageDropdown';
+export { default as RoleFilterTabs } from './RoleFilterTabs';
+export type { RoleOption } from './RoleFilterTabs';

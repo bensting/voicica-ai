3981376678234958c3897c5700c6461054a40444
bensting19-Dell <bensting19@gmail.com>
2026-02-10 09:59:24 +0700

    优化:DELL

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -207,7 +207,7 @@ export default async function RootLayout({
           id="microsoft-uet"
           strategy="lazyOnload"
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,t,u,o){w[u]=w[u]||[],o.ts=(new Date).getTime();var n=d.createElement(t);n.src="https://bat.bing.net/bat.js?ti="+o.ti+("uetq"!=u?"&q="+u:""),n.async=1,n.onload=n.onreadystatechange=function(){var s=this.readyState;s&&"loaded"!==s&&"complete"!==s||(o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad"),n.onload=n.onreadystatechange=null)};var i=d.getElementsByTagName(t)[0];i.parentNode.insertBefore(n,i)})(window,document,"script","uetq",{ti:"343232821",enableAutoSpaTracking:true});window.uetq=window.uetq||[];window.uetq.push("consent","default",{"ad_storage":"denied"});`,
+            __html: `(function(w,d,t,u,o){w[u]=w[u]||[],o.ts=(new Date).getTime();var n=d.createElement(t);n.src="https://bat.bing.net/bat.js?ti="+o.ti+("uetq"!=u?"&q="+u:""),n.async=1,n.onload=n.onreadystatechange=function(){var s=this.readyState;s&&"loaded"!==s&&"complete"!==s||(o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad"),n.onload=n.onreadystatechange=null)};var i=d.getElementsByTagName(t)[0];i.parentNode.insertBefore(n,i)})(window,document,"script","uetq",{ti:"343232821",enableAutoSpaTracking:true});window.uetq=window.uetq||[];window.uetq.push("consent","default",{"ad_storage":"granted"});`,
           }}
         />
         <SpeedInsights sampleRate={0.1} />

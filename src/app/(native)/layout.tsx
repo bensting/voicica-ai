a21faa5bbdeee9d8969a151aed5bd54ac70f39f4
Sai <benshui08@gmail.com>
2026-02-23 17:56:19 +0700

    优化

--- src/app/(native)/layout.tsx
+++ src/app/(native)/layout.tsx
@@ -70,6 +70,7 @@ export default function NativeLayout({
           {showNavbar && <NativeNavbar />}
 
           {/* 主内容区域 - PC 端为 flex 滚动区域 */}
+          {/* 主内容区域 - PC 端为 flex 滚动区域 */}
           <main className="relative lg:flex-1 lg:overflow-y-auto lg:min-h-0">{children}</main>
 
           {/* 底部导航 - 部分页面不显示 */}
